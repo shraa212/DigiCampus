64a894902281e71ad735327081cf3195b507fb86
Shravani Vetal <103918323+shraa212@users.noreply.github.com>
2024-10-09 08:02:13 +0530

    Add files via upload

---
+++ digicampus/about.php
@@ -0,0 +1,425 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>DR.D.Y PATIL INSTITUTE OF TECHNOLOGY</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600&family=Nunito:wght@600;700;800&display=swap" rel="stylesheet">
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/animate/animate.min.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+
+
+<body>
+    <!-- Spinner Start -->
+    <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+        <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+    </div>
+    <!-- Spinner End -->
+
+
+    <!-- Navbar Start -->
+    <nav class="navbar navbar-expand-lg bg-white navbar-light shadow sticky-top p-0">
+        <a href="home.php" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
+            <h2 class="m-0 text-primary" style="font-size:16px"><i class="fa fa-book me-3"></i>DR.D.Y PATIL INSTITUTE OF TECHNOLOGY</h2>
+        </a>
+        <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav ms-auto p-4 p-lg-0">
+                <a href="home.php" class="nav-item nav-link">Home</a>
+                <a href="about.php" class="nav-item nav-link active">About</a>
+                <div class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">E-content</a>
+                    <div class="dropdown-menu fade-down m-0">
+
+                        <a href="CE.php" class="dropdown-item">Civil Engineering</a>
+                        <a href="CO.php" class="dropdown-item">Computer Engineering</a>
+                        <a href="IT.php" class="dropdown-item">Information Technology</a>
+                        <a href="AIDS.php" class="dropdown-item">Artificial Intelligence and Data Science</a>
+                        <a href="AIR.php" class="dropdown-item">Artificial Intelligence And Robotics</a>
+                        <a href="EE.php" class="dropdown-item">Electrical Engineering</a>
+                        <a href="ETC.php" class="dropdown-item">Electronics and Telecommunication</a>
+                        <a href="ME.php" class="dropdown-item">Mechanical Engineering</a>
+                        <a href="IS.php" class="dropdown-item">Instrumentation Engineering</a>
+                    </div>
+                </div>
+                <a href="canteen.php" class="nav-item nav-link">Canteen</a>
+                <a href="contact.php" class="nav-item nav-link">Contact</a>
+            </div>
+            <a href="Attendance/index.php" class="btn btn-primary py-4 px-lg-5 d-none d-lg-block">Login<i class="fa fa-arrow-right ms-3"></i></a>
+        </div>
+    </nav>
+    <!-- Navbar End -->
+
+
+    <!-- Header Start -->
+    <div class="container-fluid bg-primary py-5 mb-5 page-header">
+        <div class="container py-5">
+            <div class="row justify-content-center">
+                <div class="col-lg-10 text-center">
+                    <h1 class="display-3 text-white animated slideInDown">About Us</h1>
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb justify-content-center">
+                            <li class="breadcrumb-item"><a class="text-white" href="home.php">Home</a></li>
+                            <li class="breadcrumb-item"><a class="text-white" href="#">Pages</a></li>
+                            <li class="breadcrumb-item text-white active" aria-current="page">About</li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+      <style>
+table {
+  border-collapse: collapse;
+ 
+
+}
+
+th, td {
+  padding: 8px;
+  text-align: left;
+  border-bottom: 1px solid black;
+ 
+}
+td
+{
+    color:black;
+}
+tr:nth-child(even) {background-color: #f2f2f2;}
+
+tr:hover
+{
+    background-color:#e3fefe;
+}
+</style>
+</head>
+    <!-- Header End -->
+
+
+    <!-- Service Start -->
+    <div class="container-xxl py-5">
+        <div class="container">
+            <div class="row g-4">
+                <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.1s">
+                    <div class="service-item text-center pt-3">
+                        <div class="p-4">
+                            <i class="fa fa-3x fa-graduation-cap text-primary mb-4"></i>
+                            <h5 class="mb-3">STRATEGIC VISION</h5>
+                            <p>DY Patil Institute of Technology aspires to be a global leader in technical education and research, producing innovative professionals who address global challenges and contribute to societal progress.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.3s">
+                    <div class="service-item text-center pt-3">
+                        <div class="p-4">
+                            <i class="fa fa-3x fa-globe text-primary mb-4"></i>
+                            <h5 class="mb-3">STRATEGIC MISSION</h5>
+                            <p>The institute aims to deliver quality education, foster research, and shape students into ethical professionals with the skills to solve real-world problems and meet future challenges including the concept of Emotional Intelligence.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.5s">
+                    <div class="service-item text-center pt-3">
+                        <div class="p-4">
+                            <i class="fa fa-3x fa-home text-primary mb-4"></i>
+                            <h5 class="mb-3">OUR PERSPECTIVE AND STRATEGIC PLAN</h5>
+                            <p>DY Patil focuses on holistic education by enhancing industry partnerships, research, and global exposure. Its strategic plan emphasizes critical thinking, innovation, and leadership to drive societal development.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.7s">
+                    <div class="service-item text-center pt-3">
+                        <div class="p-4">
+                            <i class="fa fa-3x fa-book-open text-primary mb-4"></i>
+                            <h5 class="mb-3">AFFILIATIONS & APPROVALS</h5>
+                            <p>Affiliated with Savitribai Phule Pune University and approved by AICTE, NBA, and DTE, the institute meets high standards in engineering education and ensures academic excellence.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Service End -->
+<!--<div class="container-xxl py-5">
+        <div class="container">
+            <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
+                <h6 class="section-title bg-white text-center text-primary px-3">OUR GOVERNING BODY</h6>
+                <h1 class="mb-5">GOVERNING BODY 2022 - 2027</h1>
+         <center>   
+    <table style="font-size: 16px;" border="1" >
+  <tr>
+     <th  style="background-color: #06BBCC;color: floralwhite;">Sr.No.</th>
+     <th style="background-color: #06BBCC;color: floralwhite;">Name</th>
+     <th style="background-color: #06BBCC;color: floralwhite;">Designation</th>
+    
+  </tr>
+  <tr>
+     <td >1.</td>
+     <td>Dr. Shankar Kisanrao Kawad<br>Hon. Chairman, Om Sai Pratishthan, Pimpalner </td>
+    <td>Chairman </td>
+  </tr>
+ <tr>
+     <td >2.</td>
+     <td>Mrs. Bharati Sunil Padwal <br>Hon. Secretary, Om Sai Pratishthan, Pimpalner </td>
+    <td>Member </td>
+  </tr>
+   <tr>
+     <td >3.</td>
+     <td>The officer & Director WRO, DTE, Mumbai.<br>DTE Nominee</td>
+    <td>Member </td>
+  </tr>
+   <tr>
+     <td >4.</td>
+     <td>Joint Director, RDTE, Nashik.<br>DTE Nominee </td>
+    <td>Member </td>
+  </tr>
+    <tr>
+     <td >5.</td>
+     <td>Dy. Secretary RBTE, Aurangabad<br>MSBTE Nominee </td>
+    <td>Member </td>
+  </tr>
+ <tr>
+     <td >6.</td>
+     <td>Dr. Amol Shashikant Shaha<br>Academician, Principal of Sitabai Thite College of Pharmacy , Shirur </td>
+    <td>Member </td>
+  </tr>
+   <tr>
+     <td >7.</td>
+     <td>Mr. Zaware Sunil Bhaskar<br>Unichem Ltd. 99, MIDC Area, Roha, District- Raigad.</td>
+    <td>Member </td>
+  </tr>
+   <tr>
+     <td >8.</td>
+     <td>Mr. Karkhile Abhijit Laxman <br>Lecturer, Om Sai Pratishthan, College of Pharmacy, Pimpalner</td>
+    <td>Member </td>
+  </tr>
+  <tr>
+     <td >9.</td>
+     <td>Mrs. Zaware Bharati Bhaskar<br>Principal, Om Sai Pratishthan, College of Pharmacy, Pimpalner</td>
+    <td>Member Secretary </td>
+  </tr>
+
+  
+     </table>
+ </center>
+ </div>
+</div>
+</div>-->
+
+
+
+    <!-- About Start -->
+    <div class="container-xxl py-5">
+        <div class="container">
+            <div class="row g-5">
+                <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s" >
+                    <div class="position-relative h-100">
+                        <img class="img-fluid position-absolute w-100 h-100" src="img/college9.jpg" alt="" style="width:300;
+                        height: 200;">
+                    </div>
+                </div>
+                <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.3s">
+                    <h6 class="section-title bg-white text-start text-primary pe-3">About Us</h6>
+                    <h1 class="mb-4">OUR VISION</h1><BR>
+                    <h4 class="mb-4">Empowerment through knowledge</h4>
+                    <p class="mb-4"></p>
+                    <p class="mb-4">
+                    At DY Patil Institute of Technology, "Empowerment Through Knowledge" is more than just a motto; it is a guiding principle that shapes the entire educational experience. The institution believes that true empowerment comes from the ability to apply knowledge creatively and responsibly to solve real-world challenges.<br><br>
+                   </p>
+
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>    
+      <div class="container-xxl py-5">
+        <div class="container">
+            <div class="row g-5">
+                <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s" >
+                    <div class="position-relative h-100">
+                        <img class="img-fluid position-absolute w-100 h-100" src="president.png" alt="" style="width:300;
+                        height: 200;">
+                    </div>
+                </div>
+                <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.3s">
+    <h1 class="mb-4">OUR MISSION</h1>
+    <p class="mb-4">Developing human potential to serve the Nation</p>
+    <ul>
+        <li>Dedicated efforts for quality education.</li>
+        <li>Yearning to promote research and development.</li>
+        <li>Persistent endeavor to imbibe moral and professional ethics.</li>
+        <li>Inculcating the concept of emotional intelligence.</li>
+        <li>Emphasizing extension work to reach out to the society.</li>
+        <li>Treading the path to meet the future challenges.</li>
+    </ul>
+    <p class="mb-4">
+        Now it is launching out in the competitive world of Technology. The team of dedicated academic and administrative staff, along with excellent infrastructural facilities, will make this organization a preferred choice for admission to an Engineering in any Domain. I am sure that the college has a bright future and is developing itself to meet the toughest challenges in the Industrial field. With Best Wishes......!
+    </p>
+</div>
+
+                    
+                </div>
+            </div>
+        </div>
+    </div>   
+     <!--<div class="container-xxl py-5 category">
+        <div class="container">
+            <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
+            <h6 class="section-title bg-white text-center text-primary px-3">Important Information</h6><br>
+            <center>
+                <img src="collegehome.png" height="400px" width="700px">
+            </center>
+             </div>
+               </div>
+    </div>-->
+    <!-- About End -->
+
+    
+ <!-- Footer Start -->
+    <form method="post">
+                <div class="container-fluid bg-dark text-light footer pt-5 mt-5 wow fadeIn" data-wow-delay="0.1s">
+        <div class="container py-5">
+            <div class="row g-5">
+                <div class="col-lg-3 col-md-6">
+                    <h4 class="text-white mb-3">Quick Link</h4>
+                    <a class="btn btn-link" href="about.php">About Us</a>
+                    <a class="btn btn-link" href="contact.php">Contact Us</a>
+                    <a class="btn btn-link" href="study.php">E-content</a>
+                    <a class="btn btn-link" href="about.php">FAQs & Help</a>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <h4 class="text-white mb-3">Contact</h4>
+                    <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>Main Campus, Sant Tukaram Nagar, Pimpri, Pune</p>
+                    <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+91 7767015942</p>
+                    <p class="mb-2"><i class="fa fa-envelope me-3"></i>info@dypatil.edu admissions@dypatil.edu</p>
+                    <div class="d-flex pt-2">
+                        <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-twitter"></i></a>
+                        <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-facebook-f"></i></a>
+                        <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-youtube"></i></a>
+                        <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-linkedin-in"></i></a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <h4 class="text-white mb-3">Our College</h4>
+                    <div class="row g-2 pt-2">
+                        <div class="col-4">
+                            <img class="img-fluid bg-light p-1" src="college1.jpg" alt="">
+                        </div>
+                        <div class="col-4">
+                            <img class="img-fluid bg-light p-1" src="college4.jpg" alt="">
+                        </div>
+                        <div class="col-4">
+                            <img class="img-fluid bg-light p-1" src="college3.jpg" alt="">
+                        </div>
+                        <div class="col-4">
+                            <img class="img-fluid bg-light p-1" src="college7.jpg" alt="">
+                        </div>
+                        <div class="col-4">
+                            <img class="img-fluid bg-light p-1" src="college5.jpg" alt="">
+                        </div>
+                        <div class="col-4">
+                            <img class="img-fluid bg-light p-1" src="college6.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <h4 class="text-white mb-3">Share Your feedback</h4>
+                    <p>We appreciate your valuable time.</p>
+                    <div class="position-relative mx-auto" style="max-width: 400px;">
+                    <input class="form-control border-0 w-100 py-3 ps-4 pe-5" type="text" name="ft" placeholder="Your Comment">
+                        <button type="submit" name="fb" class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2">Submit</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="copyright">
+                <div class="row">
+                    <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                        &copy; <a class="border-bottom" href="#">Copyright 2024 |</a> All Right Reserved.
+
+                    </a>
+                    </div>
+                    <div class="col-md-6 text-center text-md-end">
+                        <div class="footer-menu">
+                            <a href="home.php">Home</a>
+                            <a href="contact.php">Help</a>
+                            <a href="about.php">FQAs</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</form>
+    <!-- Footer End -->
+
+
+    <!-- Back to Top -->
+    <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
+
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/wow/wow.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
+    
+    <?php
+if(isset($_POST['fb']))
+{
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "digicampus";
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+$c = $_POST['ft'];
+$sql = "INSERT INTO questions (ques) VALUES ('$c')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "We will answer your queries,thank you!";
+} else {
+  echo "Error:connecting to database";
+}
+
+$conn->close();
+}
+?>
+</body>
+
+</html>
